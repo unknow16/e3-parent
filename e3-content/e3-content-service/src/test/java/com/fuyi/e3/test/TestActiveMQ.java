037b2676e13d435b0d7badd37ab4668c226d7de1
fuyi <1406751952@qq.com>
2017-11-01 14:17:44 +0800

    add test

---
+++ e3-content/e3-content-service/src/test/java/com/fuyi/e3/test/TestActiveMQ.java
@@ -0,0 +1,58 @@
+package com.fuyi.e3.test;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/applicationContext-activemq.xml"})
+public class TestActiveMQ {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private ActiveMQQueue activeMQQueue;
+
+    @Autowired
+    private ActiveMQTopic activeMQTopic;
+
+    @Test
+    public void testQueueSend() {
+        jmsTemplate.send(activeMQQueue, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+
+                TextMessage textMessage = session.createTextMessage("hellow8888");
+                return textMessage;
+            }
+        });
+    }
+
+    @Test
+    public void testTopicSend() {
+        jmsTemplate.send(activeMQTopic, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage textMessage = session.createTextMessage("hellow12222");
+                return textMessage;
+            }
+        });
+    }
+
+    @Test
+    public void testQueueRead() throws Exception {
+        System.in.read();
+    }
+}
---
+++ e3-content/e3-content-service/src/test/java/com/fuyi/e3/test/TestFastDFS.java
@@ -0,0 +1,36 @@
+package com.fuyi.e3.test;
+
+import com.fuyi.e3.common.utils.FastDFSClient;
+import org.csource.fastdfs.*;
+import org.junit.Test;
+
+public class TestFastDFS {
+
+    @Test
+    public void testUpload() throws Exception {
+        // 1、加载配置文件，配置文件中的内容就是tracker服务的地址。
+        ClientGlobal.init("E:\\workspace-idea\\e3-parent\\e3-content\\e3-content-service\\src\\main\\resources\\conf\\client.conf");
+        // 2、创建一个TrackerClient对象。直接new一个。
+        TrackerClient trackerClient = new TrackerClient();
+        // 3、使用TrackerClient对象创建连接，获得一个TrackerServer对象。
+        TrackerServer trackerServer = trackerClient.getConnection();
+        // 4、创建一个StorageServer的引用，值为null
+        StorageServer storageServer = null;
+        // 5、创建一个StorageClient对象，需要两个参数TrackerServer对象、StorageServer的引用
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+        // 6、使用StorageClient对象上传图片。
+        //扩展名不带“.”
+        String[] strings = storageClient.upload_file("C:\\Users\\Administrator\\Pictures\\90fba609e427190554e354.jpg", "jpg", null);
+        // 7、返回数组。包含组名和图片的路径。
+        for (String string: strings ) {
+            System.out.println(string);
+        }
+    }
+
+    @Test
+    public void testUploadUtil() throws Exception {
+        FastDFSClient fastDFSClient = new FastDFSClient("E:\\workspace-idea\\e3-parent\\e3-content\\e3-content-service\\src\\main\\resources\\conf\\client.conf");
+        String file = fastDFSClient.uploadFile("C:\\Users\\Administrator\\Pictures\\586b1d3dd185e.jpg");
+        System.out.println(file);
+    }
+}
